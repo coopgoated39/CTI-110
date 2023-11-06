70b883860b942bb13b181faf2cf205d4082ed5ed
coopgoated39 <145863162+coopgoated39@users.noreply.github.com>
2023-11-06 18:03:31 -0500

    Add files via upload

---
+++ P4LAB1A_CooperCurtis.py
@@ -0,0 +1,21 @@
+#Curtis Cooper
+#P4LAB1A
+#November 5, 2023
+
+import turtle
+
+sam = turtle.Turtle()
+
+sam.shape("turtle")
+
+for num in range(4):
+	sam.forward(50)
+	sam.left(90)
+
+sam.penup ()
+sam.forward(100)
+sam.pendown()
+
+for num in range (3):
+    sam.forward(50)
+    sam.left(120)
---
+++ P4LAB1B_CooperCurtis.py
@@ -0,0 +1,20 @@
+#Curtis Cooper
+#P4LAB1B
+#November 5, 2023
+
+import turtle
+sam=turtle.Turtle()
+sam.shape("turtle")
+sam.color("red")
+sam.pensize(12)
+
+sam.circle(95,-180)
+
+sam.penup()
+sam.backward(200)
+sam.pendown()
+sam.circle(95,180)
+
+
+
+
